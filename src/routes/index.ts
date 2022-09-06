3ceddac539114ebb729b786e4d074c2873970cec
SANGWOOSEE <washble@devdepot.co.kr>
2022-09-06 21:54:11 +0900

    Connect DB

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,14 @@
-import express, {Request, Response, NextFunction, Router} from 'express';
+import express, {Request, Response, NextFunction, Router, query} from 'express';
+import {DB_Connection} from '../db_connect'
 
 const router = Router();
+const db_connection = new DB_Connection();
 
 /* GET home page. */
 router.get('/', function(req: Request, res: Response, next: NextFunction) {
+  let query = 'SELECT * FROM category';
+  let result = db_connection.connection_query(query);
+
   res.render('index', { title: 'Express' });
 });
 
